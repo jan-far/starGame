92a9cea51aaee104ab6fed0e20fb40b8ee581859
jan-far <ligalifarouk@gmail.com>
2020-06-04 21:22:52 +0100

    Updates

---
+++ precache-manifest.97611832446bbdd97731b2192bf952ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdcac0b64f9b3113a23a1ee96be401a",
+    "url": "/starGame/index.html"
+  },
+  {
+    "revision": "292157b9bf128bea1d3a",
+    "url": "/starGame/static/css/main.253b9b65.chunk.css"
+  },
+  {
+    "revision": "d04d9304a612cc040db3",
+    "url": "/starGame/static/js/2.7188f5b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starGame/static/js/2.7188f5b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292157b9bf128bea1d3a",
+    "url": "/starGame/static/js/main.dc310db2.chunk.js"
+  },
+  {
+    "revision": "506bb7b38a50dee5331a",
+    "url": "/starGame/static/js/runtime-main.c3906694.js"
+  }
+]);
